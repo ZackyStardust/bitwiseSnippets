28f084da9b8d89d7b108e112b6b0ca857929f161
Felipe Pereira ramos <fpereira@c1r21s2.42lisboa.com>
2023-04-17 16:34:03 +0100

    17/04

--- swap_bits_extra.c
+++ swap_bits_extra.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 
-void swap_bits(unsigned char *byte1, unsigned char *byte2)
+void swap_bits_extra(unsigned char *byte1, unsigned char *byte2)
 {
-
+	unsigned char bits1 = (*byte1 & 0x03);
+    unsigned char bits2 = (*byte2 & 0x03);
+    
+	*byte1 = ((*byte1 >> 2) << 2) | bits2;
+    *byte2 = ((*byte2 >> 2) << 2) | bits1;
 }
